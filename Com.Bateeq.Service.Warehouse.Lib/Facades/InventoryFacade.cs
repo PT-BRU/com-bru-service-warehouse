0395f4f72816d01269254ee36676b9860106b1ee
Lucia Widyana <mo_cia@yahoo.com>
2023-05-04 13:39:57 +0700

    report stock

--- Com.Bateeq.Service.Warehouse.Lib/Facades/InventoryFacade.cs
+++ Com.Bateeq.Service.Warehouse.Lib/Facades/InventoryFacade.cs
@@ -16,8 +16,6 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Linq.Dynamic.Core;
-using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Com.Bateeq.Service.Warehouse.Lib.Facades
